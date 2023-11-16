efbcdddd078a9abc69dd836d628aad8901782c18
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2023-11-16 13:42:27 +0100

    Create CNOT.py

---
+++ Foundations/CNOT.py
@@ -0,0 +1,7 @@
+################################################################################################################################################
+#                                                                                                                                              #
+#   Autor: Dr. A. Schelle (alexej.schelle.ext@iu.org). Copyright : IU Internationale Hochschule GmbH, Juri-Gagarin-Ring 152, D-99084 Erfurt    #
+#                                                                                                                                              #
+################################################################################################################################################
+
+# PYTHON ROUTINE zur Modellierung eines GAN Diskriminators #
