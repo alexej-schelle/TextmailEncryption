51c638a6617f9432e1e0dbde0cc15810959e9e8e
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2023-11-17 15:30:43 +0100

    Update CNOT.py

--- Foundations/CNOT.py
+++ Foundations/CNOT.py
@@ -5,3 +5,20 @@
 ################################################################################################################################################
 
 # PYTHON ROUTINE zur Modellierung eines GAN Diskriminators #
+
+def CNOT(length, input_key): # Defines a pairwise CNOT-Operation
+
+    output_key = [0.0]*length
+    
+    output_key[1] = 1.0
+    output_key[2] = 1.0
+        
+    for k in range(0, M):
+
+        if (input_key[k] == 1 and input_key[k-1] == 0): input_key[k-1] = 1        
+        if (input_key[k] == 1 and input_key[k-1] == 1): input_key[k-1] = 0        
+
+        output_key[k] = input_key[k]
+        output_key[k-1] = input_key[k-1]
+
+    return output_key
